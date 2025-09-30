728438c7341b196c860721f95f19cab628eec127
miuna9890 <156643085+miuna9890@users.noreply.github.com>
2025-09-30 21:04:23 +0800

    Added question routes to retrieve all questions and questions filtered by topic and difficulty

---
+++ question-service/app/routes/question-routes.js
@@ -0,0 +1,41 @@
+import express from "express";
+import {PrismaClient} from '../../generated/prisma/index.js';
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+// GET /questions - Retrieve all questions
+router.get("/", async (req, res) => {
+    const {difficulty, topic} = req.query;
+    let questions;
+
+    try {
+        // If a difficulty or topic is provided, filter questions accordingly
+        if (difficulty || topic) {
+            questions = await prisma.question.findMany({
+                where: {
+                    ...(difficulty && {difficulty: difficulty.toUpperCase()}),
+                    ...(topic && {topic: topic}),
+                },
+            });
+
+            if (questions.length === 0) {
+                return res.status(404).json({error: "No questions found matching the criteria"});
+            }
+
+            //pick one random question from the filtered list
+            const randomIndex = Math.floor(Math.random() * questions.length);
+            res.json(questions[randomIndex]);
+
+        } else {
+            // If no filters, return the first 100 questions
+            questions = await prisma.question.findMany({take: 100});
+            res.json(questions);
+        }
+    } catch (error) {
+        console.error("Error fetching questions:", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+});
+
+export default router;
