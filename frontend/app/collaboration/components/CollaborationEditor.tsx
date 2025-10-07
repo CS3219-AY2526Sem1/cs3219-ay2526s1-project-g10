0158babb730d6b5859d5bd2543b1cc579d754c00
nicleongyj <e0968256@u.nus.edu>
2025-10-07 13:53:03 +0800

    Add code execution API

--- frontend/app/collaboration/components/CollaborationEditor.tsx
+++ frontend/app/collaboration/components/CollaborationEditor.tsx
@@ -6,6 +6,7 @@ import MonacoEditor from "@monaco-editor/react"
 import * as Y from "yjs"
 import { MonacoBinding } from "y-monaco"
 import { WebsocketProvider } from "y-websocket"
+import axios from "axios"
 
 import {
   DropdownMenu,
@@ -15,8 +16,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../../../components/ui/dropdown-menu"
+import { CODE_VERSIONS } from "../../../lib/constants"
 
-import { ChevronDownIcon } from "lucide-react"
+import { ChevronDownIcon, Play } from "lucide-react"
+import { Button } from "../../../components/ui/button"
+import { version } from "os"
 
 
 const LANGUAGES = [
@@ -25,8 +29,15 @@ const LANGUAGES = [
   { label: "C++", value: "cpp" },
 ]
 
+const API = axios.create({
+  baseURL: "https://emkc.org/api/v2/piston",
+})
+
+
 export default function CollaborationEditor({ roomId }: { roomId: string | null }) {
   const [language, setLanguage] = useState("python")
+  const [codeRunning, setCodeRunning] = useState(false);
+  const [codeOutput, setCodeOutput] = useState<string | null>(null);
   const editorRef = useRef<any>(null)
   const ydocRef = useRef<Y.Doc | null>(null)
   const providerRef = useRef<WebsocketProvider | null>(null)
@@ -72,6 +83,39 @@ export default function CollaborationEditor({ roomId }: { roomId: string | null 
     })
   }
 
+  const handleCodeRun = async () => {
+    if (!editorRef.current) return
+    const code = editorRef.current.getValue()
+  
+    try {
+      console.log("Running code in language:", language)
+      setCodeRunning(true);
+      
+      const response = await API.post("/execute", {
+        language: language,
+        version : CODE_VERSIONS[language],
+        files: [
+          {
+            name: `Main.${language === "python" ? "py" : language === "java" ? "java" : "cpp"}`,
+            content: code
+          }
+        ],
+        stdin: "",
+        args: [],
+        compile_timeout: 10000,
+        run_timeout: 3000,
+        compile_memory_limit: -1,
+      })
+      
+      const result = response.data.run.output
+      setCodeRunning(false);
+      setCodeOutput(result);
+    } catch (error) {
+      alert("Error running code")
+      console.error("Error:", error)
+    }
+  }
+
   return (
     <div className="w-1/2 flex items-center justify-center bg-slate-800 h-full">
       <div className="bg-blue-200 rounded-lg shadow-lg p-6 w-[90%] h-[95%] flex flex-col">
@@ -98,24 +142,36 @@ export default function CollaborationEditor({ roomId }: { roomId: string | null 
         </DropdownMenu>
       </div>
         <div className="flex-1 flex items-center justify-center">
-          <div className="w-full h-full flex items-center justify-center">
-            <div className="w-[95%] h-[90%]">
-              <MonacoEditor
-                height="100%"
-                width="100%"
-                language={language}
-                theme="vs-dark"
-                defaultValue="// Write your code here"
-                onMount={handleEditorDidMount}
-                options={{
-                  fontSize: 16,
-                  minimap: { enabled: false },
-                  scrollbar: { vertical: "visible", horizontal: "visible" },
-                  padding: { top: 10, bottom: 10 },
-                }}
-              />
-            </div>
+        <div className="w-full h-full flex flex-col">
+          <div className="flex items-center justify-end bg-gray-900 px-4 py-2 rounded-t-xl">
+            <Button
+              onClick={() => handleCodeRun()}
+              variant="ghost"
+              size="sm"
+              className="bg-gray-800 text-gray-100 hover:bg-gray-700"
+            >
+              Run
+              <Play className="ml-1 h-4 w-4" />
+            </Button>
           </div>
+          {/* Code editor fills the rest */}
+          <div className="flex-1 w-full h-full">
+            <MonacoEditor
+              height="100%"
+              width="100%"
+              language={language}
+              theme="vs-dark"
+              defaultValue="// Write your code here"
+              onMount={handleEditorDidMount}
+              options={{
+                fontSize: 16,
+                minimap: { enabled: false },
+                scrollbar: { vertical: "visible", horizontal: "visible" },
+                padding: { top: 10, bottom: 10 },
+              }}
+            />
+          </div>
+        </div>
         </div>
       </div>
     </div>
