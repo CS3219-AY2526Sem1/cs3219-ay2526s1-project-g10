aa61f1b389ba88e0bcc522da9d1df06fc30ef8a3
nicleongyj <e0968256@u.nus.edu>
2025-10-07 12:56:36 +0800

    Change code language

--- frontend/app/collaboration/components/CollaborationEditor.tsx
+++ frontend/app/collaboration/components/CollaborationEditor.tsx
@@ -20,8 +20,6 @@ import { ChevronDownIcon } from "lucide-react"
 
 
 const LANGUAGES = [
-  { label: "JavaScript", value: "javascript" },
-  { label: "TypeScript", value: "typescript" },
   { label: "Python", value: "python" },
   { label: "Java", value: "java" },
   { label: "C++", value: "cpp" },
