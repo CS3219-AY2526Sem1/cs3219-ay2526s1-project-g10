bcc504878209398600e60c55f01724de7df42692
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-10-17 17:09:50 +0800

    Connect to supabase

--- user-service/supabaseClient.js
+++ user-service/supabaseClient.js
@@ -1,5 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+import dotenv from "dotenv";
+
+dotenv.config(); //Load environment variables
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
