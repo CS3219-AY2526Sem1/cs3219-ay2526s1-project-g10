f40bf7f38f5c8bbbceed4ee2fe43d4db9c02fa7a
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-10-17 21:55:47 +0800

    Add authentication test routes

--- user-service/app/server.js
+++ user-service/app/server.js
@@ -3,6 +3,9 @@ import index from "./index.js";
 import "dotenv/config";
 import { connectToDB } from "./model/repository.js";
 
+import testRoutes from "./app/routes/test-routes.js";
+app.use("/test", testRoutes);
+
 const port = process.env.USER_SERVICE_PORT || 3001;
 
 const server = http.createServer(index);
