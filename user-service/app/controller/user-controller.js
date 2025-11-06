5bff9a720eb0b9c87fc3ac2c9b669d209e8a7007
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-11-06 23:26:59 +0800

    remove mongoDB imports

--- user-service/app/controller/user-controller.js
+++ user-service/app/controller/user-controller.js
@@ -1,5 +1,4 @@
 import bcrypt from "bcrypt";
-import { isValidObjectId } from "mongoose";
 import {
   createUser as _createUser,
   deleteUserById as _deleteUserById,
@@ -24,6 +23,7 @@ export async function createUser(req, res) {
       const salt = bcrypt.genSaltSync(10);
       const hashedPassword = bcrypt.hashSync(password, salt);
       const createdUser = await _createUser(username, email, hashedPassword);
+
       return res.status(201).json({
         message: `Created new user ${username} successfully`,
         data: formatUserResponse(createdUser),
@@ -40,26 +40,23 @@ export async function createUser(req, res) {
 export async function getUser(req, res) {
   try {
     const userId = req.params.id;
-    if (!isValidObjectId(userId)) {
+    const user = await _findUserById(userId);
+
+    if (!user) {
       return res.status(404).json({ message: `User ${userId} not found` });
     }
 
-    const user = await _findUserById(userId);
-    if (!user) {
-      return res.status(404).json({ message: `User ${userId} not found` });
-    } else {
-      return res.status(200).json({ message: `Found user`, data: formatUserResponse(user) });
-    }
+    return res.status(200).json({ message: `Found user`, data: formatUserResponse(user) });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: "Unknown error when getting user!" });
+    // This will catch invalid UUIDs too
+    return res.status(400).json({ message: "Invalid user ID or query failed" });
   }
 }
 
 export async function getAllUsers(req, res) {
   try {
     const users = await _findAllUsers();
-
     return res.status(200).json({ message: `Found users`, data: users.map(formatUserResponse) });
   } catch (err) {
     console.error(err);
@@ -70,42 +67,42 @@ export async function getAllUsers(req, res) {
 export async function updateUser(req, res) {
   try {
     const { username, email, password } = req.body;
-    if (username || email || password) {
-      const userId = req.params.id;
-      if (!isValidObjectId(userId)) {
-        return res.status(404).json({ message: `User ${userId} not found` });
-      }
-      const user = await _findUserById(userId);
-      if (!user) {
-        return res.status(404).json({ message: `User ${userId} not found` });
-      }
-      if (username || email) {
-        let existingUser = await _findUserByUsername(username);
-        if (existingUser && existingUser.id !== userId) {
-          return res.status(409).json({ message: "username already exists" });
-        }
-        existingUser = await _findUserByEmail(email);
-        if (existingUser && existingUser.id !== userId) {
-          return res.status(409).json({ message: "email already exists" });
-        }
-      }
-
-      let hashedPassword;
-      if (password) {
-        const salt = bcrypt.genSaltSync(10);
-        hashedPassword = bcrypt.hashSync(password, salt);
-      }
-      const updatedUser = await _updateUserById(userId, username, email, hashedPassword);
-      return res.status(200).json({
-        message: `Updated data for user ${userId}`,
-        data: formatUserResponse(updatedUser),
-      });
-    } else {
-      return res.status(400).json({ message: "No field to update: username and email and password are all missing!" });
+    if (!username && !email && !password) {
+      return res.status(400).json({ message: "No field to update: username, email, and password are all missing!" });
     }
+
+    const userId = req.params.id;
+    const user = await _findUserById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: `User ${userId} not found` });
+    }
+
+    if (username || email) {
+      let existingUser = await _findUserByUsername(username);
+      if (existingUser && existingUser.id !== userId) {
+        return res.status(409).json({ message: "username already exists" });
+      }
+      existingUser = await _findUserByEmail(email);
+      if (existingUser && existingUser.id !== userId) {
+        return res.status(409).json({ message: "email already exists" });
+      }
+    }
+
+    let hashedPassword;
+    if (password) {
+      const salt = bcrypt.genSaltSync(10);
+      hashedPassword = bcrypt.hashSync(password, salt);
+    }
+
+    const updatedUser = await _updateUserById(userId, username, email, hashedPassword);
+    return res.status(200).json({
+      message: `Updated data for user ${userId}`,
+      data: formatUserResponse(updatedUser),
+    });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: "Unknown error when updating user!" });
+    return res.status(400).json({ message: "Invalid user ID or query failed" });
   }
 }
 
@@ -113,37 +110,33 @@ export async function updateUserPrivilege(req, res) {
   try {
     const { isAdmin } = req.body;
 
-    if (isAdmin !== undefined) {  // isAdmin can have boolean value true or false
-      const userId = req.params.id;
-      if (!isValidObjectId(userId)) {
-        return res.status(404).json({ message: `User ${userId} not found` });
-      }
-      const user = await _findUserById(userId);
-      if (!user) {
-        return res.status(404).json({ message: `User ${userId} not found` });
-      }
-
-      const updatedUser = await _updateUserPrivilegeById(userId, isAdmin === true);
-      return res.status(200).json({
-        message: `Updated privilege for user ${userId}`,
-        data: formatUserResponse(updatedUser),
-      });
-    } else {
+    if (isAdmin === undefined) {
       return res.status(400).json({ message: "isAdmin is missing!" });
     }
+
+    const userId = req.params.id;
+    const user = await _findUserById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: `User ${userId} not found` });
+    }
+
+    const updatedUser = await _updateUserPrivilegeById(userId, isAdmin === true);
+    return res.status(200).json({
+      message: `Updated privilege for user ${userId}`,
+      data: formatUserResponse(updatedUser),
+    });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: "Unknown error when updating user privilege!" });
+    return res.status(400).json({ message: "Invalid user ID or query failed" });
   }
 }
 
 export async function deleteUser(req, res) {
   try {
     const userId = req.params.id;
-    if (!isValidObjectId(userId)) {
-      return res.status(404).json({ message: `User ${userId} not found` });
-    }
     const user = await _findUserById(userId);
+
     if (!user) {
       return res.status(404).json({ message: `User ${userId} not found` });
     }
@@ -152,7 +145,7 @@ export async function deleteUser(req, res) {
     return res.status(200).json({ message: `Deleted user ${userId} successfully` });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ message: "Unknown error when deleting user!" });
+    return res.status(400).json({ message: "Invalid user ID or query failed" });
   }
 }
 
